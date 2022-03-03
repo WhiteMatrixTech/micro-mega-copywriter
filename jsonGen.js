667b031b9b233595b7c5d6440cdd8be4de8ca699
nemo <18136225579@163.com>
2022-03-03 16:16:29 +0800

    feat: modify deploy config

---
+++ jsonGen.js
@@ -0,0 +1,36 @@
+let xlsx = require('xlsx');
+let fs = require('fs')
+let workbook = xlsx.readFile('./GCA Founding Councilors.xlsx');
+
+let sheetNames = workbook.SheetNames;
+
+let sheet = workbook.Sheets[sheetNames[0]];
+
+const data = xlsx.utils.sheet_to_json(sheet);
+
+const fileGenTemplate = (councilors) => {
+    return `export const copywriter = {
+        councilors: ${JSON.stringify(councilors)}
+    }`
+}
+
+const parseData = data.map(item => {
+    const mediaAccounts = [{
+        name: 'Twitter',
+        link: item.Twitter
+    }, {
+        name: 'Email',
+        link: item.Email
+    }, {
+        name: 'Discord',
+        link: item.Discord
+    }].filter(item => !!item.link)
+
+    return {
+        name: item.Name,
+        role: item.Roles,
+        mediaAccounts
+    }
+})
+
+fs.writeFile("./index.ts", fileGenTemplate(parseData), () => {});
